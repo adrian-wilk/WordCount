fc876ff1a35db06e49b6fb700013fac19478d336
Adrian Wilk <awilk2@gmail.com>
2020-04-08 14:57:41 +0200

    WordCount class finished and working.

--- src/WordCount.java
+++ src/WordCount.java
@@ -6,6 +6,14 @@ public class WordCount {
 
     public static void main(String [] args) throws Exception{
         File file = new File("file.txt");
-Scanner scanner = new Scanner(file);
+        Scanner scanner = new Scanner(file);
+
+        int words = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            words += (line.split(" ").length);
+        }
+
+        System.out.println("The file contains: " + words + " words");
     }
 }
