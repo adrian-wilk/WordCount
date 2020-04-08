3236670fa27fe338fbf95dc82e5f51960a8f4edc
Adrian Wilk <awilk2@gmail.com>
2020-04-08 14:50:21 +0200

    WordCount class skeleton added + file.txt added

---
+++ src/WordCount.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.File;
+
+
+public class WordCount {
+
+    public static void main(String [] args) throws Exception{
+        File file = new File("file.txt");
+Scanner scanner = new Scanner(file);
+    }
+}
